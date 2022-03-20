72bb234d7350d0ade35b28ed21b5fc1e7ea90d4f
Michael Franco <franco.luis.michael@gmail.com>
2022-03-20 14:09:09 -0500

    commit tree e0acf97abc39776a2e5a9bad5c858b8903403f88

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string){
+    const result = drivers.filter(drivers => drivers === string || drivers === string.toLowerCase())
+    return result
+
+}
+
+function fuzzyMatch(drivers, string){
+    let firstLetter = drivers.filter(drivers => drivers.slice(0, string.lengeth) === string)
+    return firstLetter
+}
